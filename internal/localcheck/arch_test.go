9fe5868b0b860e0b378f3205d96bd1cb49864733
Harry Zheng <harry@harryzheng.com>
2021-04-03 14:38:17 +0800

    Unit test architectures

--- internal/localcheck/arch_test.go
+++ internal/localcheck/arch_test.go
@@ -55,3 +55,35 @@ func TestArchitecture_LoadFat(t *testing.T) {
 		assert.Equal(t, test.expected, arch)
 	}
 }
+
+func TestArchitecture_String(t *testing.T) {
+	tests := []struct {
+		arch     Architectures
+		expected string
+	}{
+		{Architectures{PowerPC: 0b01}, "PowerPC 32"},
+		{Architectures{PowerPC: 0b10}, "PowerPC 64"},
+		{Architectures{PowerPC: 0b11}, "PowerPC 32/64"},
+		{Architectures{Intel: 0b01}, "Intel 32"},
+		{Architectures{Intel: 0b10}, "Intel 64"},
+		{Architectures{Intel: 0b11}, "Intel 32/64"},
+		{Architectures{Arm: 0b01}, "Arm 32"},
+		{Architectures{Arm: 0b10}, "Arm 64"},
+		{Architectures{Arm: 0b11}, "Arm 32/64"},
+
+		{Architectures{Intel: 0b10, Arm: 0b10}, "Intel 64, Arm 64"},
+		{Architectures{Intel: 0b11, Arm: 0b10}, "Intel 32/64, Arm 64"},
+		{Architectures{PowerPC: 0b11, Arm: 0b11}, "PowerPC 32/64, Arm 32/64"},
+
+		{Architectures{}, "Unknown"},
+	}
+
+	for _, test := range tests {
+		actual := test.arch.String()
+		assert.Equal(t, test.expected, actual)
+	}
+}
+
+func TestGetBitString_EdgeCase(t *testing.T) {
+	assert.Empty(t, getBitString(0))
+}
