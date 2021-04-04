00fefbd2ca17628ea0b7eef830331b22dd9ed9b9
Harry Zheng <harry@harryzheng.com>
2021-04-04 08:09:13 +0800

    Test output package

--- internal/output/output.go
+++ internal/output/output.go
@@ -1,15 +1,18 @@
 package output
 
 import (
+	"io"
 	"os"
 
 	"github.com/harryzcy/ascheck/internal/macapp"
 	"github.com/olekukonko/tablewriter"
 )
 
+var out io.Writer = os.Stdout
+
 // Table prints application information in table format.
 func Table(apps []macapp.Application) {
-	table := tablewriter.NewWriter(os.Stdout)
+	table := tablewriter.NewWriter(out)
 	table.SetHeader([]string{"Name", "Current Architectures", "Arm Support"})
 	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
 	table.SetCenterSeparator("")
